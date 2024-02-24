02e07c363579e9e2895cf9269e1d866bdf2ff7d5
qqAndrey <63346175+botmadness111@users.noreply.github.com>
2024-02-24 13:21:33 +0300

    Lambda Выражения + Stream

--- src/Main.java
+++ src/Main.java
@@ -1,57 +1,23 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
-
-@FunctionalInterface
-interface MySortInterface {
-    List<Integer> sort(List<Integer> arr);
-}
-
-@FunctionalInterface
-interface Executor {
-    int execute(int x);
-}
-
-class Runner {
-    public void run(Executor executor, int value) {
-        int ans = executor.execute(value);
-        System.out.println(ans);
-    }
-}
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
-        List<Integer> array = new ArrayList<>(List.of(2, 1, 3, 5, 12, 6));
+        List<Integer> list = new ArrayList<>(List.of(2, 1, 3, 5, 7, 1));
 
-        MySortInterface mySortInterface = ((x) -> {
-            Collections.sort(x);
-            return x;
-        });
+        list = list.stream().map(x -> x * 2).collect(Collectors.toList());
+        System.out.println(list);
 
-        array = mySortInterface.sort(array);
-        System.out.println(array);
+        Set<Integer> set = new HashSet<>(Set.of(1, 2, 3, 4, 5, 6));
+        set = set.stream().map(x -> x * 3).collect(Collectors.toSet());
+        System.out.println(set);
 
-//        ---------------------------------------------------------------------------
+        list = list.stream().filter(x -> x >= 10).collect(Collectors.toList());
+        System.out.println(list);
 
-        Runner runner = new Runner();
+//        list.forEach(System.out::println);
 
-        int value = 7;
-        runner.run((x) -> x + 10, value);
+        System.out.println( list.stream().reduce((x, y) -> x+y).get());
 
-        //        ---------------------------------------------------------------------------
-
-        List<Integer> arr = new ArrayList<>(List.of(3, 2, 1));
-
-//        arr.sort(new Comparator<Integer>() {
-//            @Override
-//            public int compare(Integer o1, Integer o2) {
-//                return o1 - o2;
-//            }
-//        });
-
-        arr.sort((Integer x1, Integer x2) -> x2 - x1);
-
-        System.out.println(arr);
     }
 }
